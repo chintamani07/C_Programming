aa442949531d558c30ed6e88f61c8608b69179fc
Chintamani Wankhede <125362482+chintamani07@users.noreply.github.com>
2024-02-07 20:34:41 +0530

    Add files via upload

--- 2d_array.c
+++ 2d_array.c
@@ -8,7 +8,7 @@ int main()
     // 2D array initialization
     int rollno[3][3]={{12,11,13},{2,3,4}};
     
-    printf("The element at 0th index : %d\n",rollno[0][0]);
-    printf("The element at 1st index : %d",rollno[1][0]);
+    printf("The element at 0 0th index : %d\n",rollno[0][0]);
+    printf("The element at 1 0th index : %d",rollno[1][0]);
     return 0;
 }
