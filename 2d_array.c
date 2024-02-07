69b8631110981a94c19fedc9c8f2be652895410e
Chintamani Wankhede <125362482+chintamani07@users.noreply.github.com>
2024-02-07 20:31:57 +0530

    Add files via upload

---
+++ 2d_array.c
@@ -0,0 +1,14 @@
+// Write a C program to print 2D Array.
+
+// standard i/o header file
+#include<stdio.h>
+// main function
+int main()
+{
+    // 2D array initialization
+    int rollno[3][3]={{12,11,13},{2,3,4}};
+    
+    printf("The element at 0th index : %d\n",rollno[0][0]);
+    printf("The element at 1st index : %d",rollno[1][0]);
+    return 0;
+}
