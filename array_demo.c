5dee27a8de8cd3f21cb8919bac034e0cbc694e16
Chintamani Wankhede <125362482+chintamani07@users.noreply.github.com>
2024-02-08 13:09:59 +0530

    Add files via upload

---
+++ array_demo.c
@@ -0,0 +1,13 @@
+// Write a C program to declare 1D Array.
+
+// standard i/o header file
+#include<stdio.h>
+// int main
+int main()
+{
+    // 1D array initialization
+    int marks[4] = {11,22,44,66};
+
+    printf("Array Element is : %d",marks[0]);
+    return 0;
+}
